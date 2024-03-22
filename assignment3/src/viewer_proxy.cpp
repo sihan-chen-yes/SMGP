fa2cc8284bb47e671466839d85143fcfdb277294
Floor Verhoeven <vfloor@student.ethz.ch>
2024-03-22 10:42:37 +0100

    Release assignment 3

---
+++ assignment3/src/viewer_proxy.cpp
@@ -0,0 +1,78 @@
+#include "viewer_proxy.h"
+
+#include <igl/opengl/glfw/Viewer.h>
+#include <igl/opengl/glfw/imgui/ImGuiHelpers.h>
+#include <igl/opengl/glfw/imgui/ImGuiMenu.h>
+
+igl::opengl::glfw::Viewer viewer;
+igl::opengl::glfw::imgui::ImGuiPlugin plugin;
+igl::opengl::glfw::imgui::ImGuiMenu menu;
+ViewerProxy* ViewerProxy::_instance = nullptr;
+
+static void init_igl_viewer() {
+  viewer.plugins.push_back(&plugin);
+  plugin.widgets.push_back(&menu);
+}
+
+ViewerProxy& ViewerProxy::get_instance() {
+  if (_instance == nullptr) {
+    _instance = new ViewerProxy();
+    init_igl_viewer();
+
+    viewer.callback_key_down = [&](igl::opengl::glfw::Viewer& viewer,
+                                   unsigned char key, int modifiers) {
+      return _instance->callback_key_down(*_instance, key, modifiers);
+    };
+  }
+  return *_instance;
+}
+
+void ViewerProxy::launch() { viewer.launch(); }
+
+void ViewerProxy::Data::clear() { viewer.data().clear(); }
+void ViewerProxy::Data::compute_normals() { viewer.data().compute_normals(); }
+void ViewerProxy::Data::set_mesh(const Eigen::MatrixXd& V,
+                                 const Eigen::MatrixXi& F) {
+  viewer.data().set_mesh(V, F);
+}
+void ViewerProxy::Data::set_normals(const Eigen::MatrixXd& N) {
+  viewer.data().set_normals(N);
+}
+void ViewerProxy::Data::set_colors(const Eigen::MatrixXd& colors) {
+  viewer.data().set_colors(colors);
+}
+
+void ViewerProxy::Data::add_points(const Eigen::MatrixXd& P,
+                                   const Eigen::MatrixXd& C) {
+  viewer.data().add_points(P, C);
+}
+
+void ViewerProxy::Data::set_edges(const Eigen::MatrixXd& P,
+                                  const Eigen::MatrixXi& E,
+                                  const Eigen::MatrixXd& C) {
+  viewer.data().set_edges(P, E, C);
+}
+
+void ViewerProxy::Data::add_edges(const Eigen::MatrixXd& P1,
+                                  const Eigen::MatrixXd& P2,
+                                  const Eigen::MatrixXd& C) {
+    viewer.data().add_edges(P1, P2, C);
+}
+
+void ViewerProxy::Core::align_camera_center(const Eigen::MatrixXd& V,
+                                            const Eigen::MatrixXi& F) {
+  viewer.core().align_camera_center(V, F);
+}
+
+void ViewerProxy::Core::align_camera_center(const Eigen::MatrixXd& P) {
+  viewer.core().align_camera_center(P);
+}
+
+ViewerProxy::Data::Data()
+    : point_size(viewer.data().point_size),
+      show_lines(viewer.data().show_lines),
+      show_faces(viewer.data().show_faces) {}
+
+ViewerProxy::Menu::Menu()
+    : callback_draw_viewer_menu(::menu.callback_draw_viewer_menu) {}
+void ViewerProxy::Menu::draw_viewer_menu() { ::menu.draw_viewer_menu(); }
---
+++ assignment3/src/viewer_proxy.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <Eigen/Eigen>
+#include <functional>
+
+// Simple proxy around libigl's Viewer to cache the heavy compilation.
+// We try to include all functions from libigl's Viewer
+// (https://github.com/libigl/libigl/blob/main/include/igl/opengl/ViewerData.h)
+// that you need, but we may have missed something. In that case you can add a
+// the function here and in viewer_proxy.cpp following the examples that are
+// already there. Editing the viewer_proxy.h/.cpp files will significantly slow
+// down your compilation so we recommend to avoid editing it frequently.
+
+class ViewerProxy {
+ public:
+  // Proxy for igl::opengl::ViewerData.
+  class Data {
+   public:
+    Data();
+    void clear();
+    void compute_normals();
+    void set_mesh(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F);
+    void set_normals(const Eigen::MatrixXd& N);
+    void set_colors(const Eigen::MatrixXd& colors);
+    void add_points(const Eigen::MatrixXd& P, const Eigen::MatrixXd& C);
+    void add_edges(const Eigen::MatrixXd& P1, const Eigen::MatrixXd& P2,
+                   const Eigen::MatrixXd& C);
+    void set_edges(const Eigen::MatrixXd& P, const Eigen::MatrixXi& E,
+                const Eigen::MatrixXd& C);
+
+    float& point_size;
+    unsigned int& show_lines;
+    unsigned int& show_faces;
+  };
+  // Proxy for igl::opengl::ViewerCore.
+  class Core {
+   public:
+    void align_camera_center(const Eigen::MatrixXd& V,
+                             const Eigen::MatrixXi& F);
+    void align_camera_center(const Eigen::MatrixXd& P);
+  };
+
+  // Proxy for igl::opengl::glfw::imgui::ImGuiMenu.
+  class Menu {
+   public:
+    Menu();
+    void draw_viewer_menu();
+
+    std::function<void(void)>& callback_draw_viewer_menu;
+  };
+
+  static ViewerProxy& get_instance();
+
+  void launch();
+  Data& data() { return _data; }
+  Core& core() { return _core; }
+  Menu& menu() { return _menu; }
+
+  std::function<bool(ViewerProxy& viewer, unsigned int key, int modifiers)>
+      callback_key_down = [&](ViewerProxy& viewer, unsigned int key,
+                              int modifiers) { return true; };
+
+ protected:
+  ViewerProxy() {}
+  static ViewerProxy* _instance;
+  Data _data;
+  Core _core;
+  Menu _menu;
+};
